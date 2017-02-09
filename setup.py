50a96ffdacbc3096a551709afacbbc18a5247e01
Guillaume Pasquet <guillaume.pasquet@nccgroup.trust>
2017-02-09 15:35:33 +0000

    Fix requirements in setup.py.

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@
 
 import os
 from setuptools import setup
-from pip.req import parse_requirements
-
-reqs_path = os.path.dirname(os.path.realpath(__file__)) + '/requirements/release.txt'
-install_reqs = parse_requirements(reqs_path, session=False)
-requires = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='ncc_paapi',
@@ -16,6 +11,6 @@ setup(
     description='Abstraction classes to access the PA API',
     author='NCC Group',
     packages=['paapi'],
-    install_requires=requires,
+    install_requires=['urllib3==1.19.1'],
     url='https://github.com/ncc-tools/python-pa-api'
 )
